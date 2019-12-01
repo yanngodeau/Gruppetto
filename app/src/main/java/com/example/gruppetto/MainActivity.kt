35ea2483b8192696b2cb0cab01ef34cdb290e538
Yann Godeau <yann.godeau@gmail.com>
2019-12-01 01:42:53 +0100

    Location and address detection

--- app/src/main/java/com/example/gruppetto/MainActivity.kt
+++ app/src/main/java/com/example/gruppetto/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.gruppetto
 
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -15,6 +18,15 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
+import com.google.android.libraries.places.api.net.PlacesClient
+import java.io.IOException
+import java.util.*
+
 
 open class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
@@ -25,8 +37,9 @@ open class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationTextView: TextView
+    private lateinit var addressTextView: TextView
     private lateinit var lastLocation: Location
-
+    private lateinit var placesClient: PlacesClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +50,10 @@ open class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
         mapFragment.getMapAsync(this)
 
         locationTextView = findViewById(R.id.current_location)
+        addressTextView = findViewById(R.id.current_address)
+
+        setUpPlaces()
+        guessCurrentPlace()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -47,11 +64,18 @@ open class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
         map.addMarker(MarkerOptions().position(paulSabatier).title("Paul Sabatier"))
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(paulSabatier, 17.0f))
 
-
         setUpMap()
     }
 
     override fun onMarkerClick(p0: Marker?) = false
+
+    private fun setUpPlaces() {
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, resources.getString(R.string.google_maps_key))
+        }
+
+        placesClient = Places.createClient(this)
+    }
 
     private fun setUpMap() {
         if (ActivityCompat.checkSelfPermission(
@@ -73,10 +97,54 @@ open class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnM
             if (location != null) {
                 lastLocation = location
                 val currentLatLng = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLng)
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 16f))
             }
         }
+    }
 
+    private fun placeMarkerOnMap(latLng: LatLng) {
+        val markerOptions = MarkerOptions().position(latLng)
 
+        val titleStr = getAddress(latLng)  // add these two lines
+        addressTextView.text = titleStr
+        markerOptions.title(titleStr)
+
+        map.addMarker(markerOptions)
+    }
+
+    private fun getAddress(latLng: LatLng): String {
+        val geocoder = Geocoder(this)
+        val addresses: List<Address>?
+        val address: Address?
+        var addressText = ""
+
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+            if (null != addresses && addresses.isNotEmpty()) {
+                addressText =
+                    addresses.get(0).featureName + ", " + addresses.get(0).adminArea + ", " + addresses.get(
+                        0
+                    ).countryName
+            }
+        } catch (e: IOException) {
+            Log.e("MapsActivity", e.localizedMessage)
+        }
+
+        return addressText
+    }
+
+    private fun guessCurrentPlace() {
+        val placeFields: List<Place.Field> = Collections.singletonList(Place.Field.NAME)
+        val currentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
+
+        var currentPlaceTask: Task<FindCurrentPlaceResponse> = placesClient.findCurrentPlace(currentPlaceRequest)
+        currentPlaceTask.addOnSuccessListener { response ->
+            locationTextView.text = response.placeLikelihoods[0].place.name
+        }
+        currentPlaceTask.addOnFailureListener { exception ->
+            exception.printStackTrace()
+            locationTextView.text = "Error Place"
+        }
     }
 }
