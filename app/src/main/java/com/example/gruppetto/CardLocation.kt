046502d75e1c337b69c94c27c4b3bae34282d7ca
Yann Godeau <yann.godeau@gmail.com>
2019-12-02 14:29:52 +0100

    Location list view

---
+++ app/src/main/java/com/example/gruppetto/CardLocation.kt
@@ -0,0 +1,23 @@
+package com.example.gruppetto
+
+open class CardLocation {
+    var location: String
+    var address: String
+    lateinit var date: String
+
+    private lateinit var locations: List<CardLocation>
+
+    constructor(location: String, address: String, date: String) {
+        this.location = location
+        this.address = address
+        this.date = date
+    }
+
+    constructor(location: String, address: String) {
+        this.location = location
+        this.address = address
+    }
+
+    private fun initializeData() {
+    }
+}
---
+++ app/src/main/java/com/example/gruppetto/LocationAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.gruppetto
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class LocationAdapter(private val context: Context,
+                      private val dataSource: ArrayList<CardLocation>): BaseAdapter() {
+
+    private val inflater: LayoutInflater
+            = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val rowView = inflater.inflate(R.layout.list_item_location, parent, false)
+        val locationText = rowView.findViewById<TextView>(R.id.current_location)
+        val addressText = rowView.findViewById<TextView>(R.id.current_address)
+
+        val location = getItem(position)
+        locationText.text = location.location
+        addressText.text = location.address
+        return rowView
+    }
+
+    override fun getItem(position: Int): CardLocation {
+        return dataSource[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+}
