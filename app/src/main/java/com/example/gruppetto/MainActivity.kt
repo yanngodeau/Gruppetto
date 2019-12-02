15da0e5c02c9d9eb2e05f6c86095dc463cb2c0b0
matt01590 <matt01590@gmail.com>
2019-12-02 13:08:27 +0100

    lien avec FirebaseAuth et FireStore, lecture partielle du profil authentification

--- app/src/main/java/com/example/gruppetto/MainActivity.kt
+++ app/src/main/java/com/example/gruppetto/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.gruppetto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var profilButton : Button = findViewById(R.id.buttonProfil)
+        profilButton.setOnClickListener { openProfil() }
+    }
+
+    private fun openProfil() {
+        val intent = Intent(this, ProfilActivity::class.java)
+        startActivity(intent)
     }
 }
